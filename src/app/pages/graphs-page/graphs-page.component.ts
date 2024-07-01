d58906f9aaf7107d9481baa9cc1fb3439fba9db5
Etienne Hüsler <ehusler@lab4tech.lan>
2024-07-01 15:58:37 +0200

    Refactored graph page

--- src/app/pages/graphs-page/graphs-page.component.ts
+++ src/app/pages/graphs-page/graphs-page.component.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core'
 import { Chart } from 'chart.js/auto' 
 import { CollectionsService } from '../../services/collections.service'
 import { Collection } from '../../data-models/collection.model'
-import { UrzaCard } from '../../data-models/urza-card.model'
+import { GraphsPageService } from './graphs-page.service'
 
 @Component({
   selector: 'app-graphs-page',
@@ -18,14 +18,13 @@ export class GraphsPageComponent implements AfterViewInit{
   collection1?: Collection
   collection2?: Collection
 
-  graphLabels?: string[]
-  graphData1?: number[]
-  graphData2?: number[]
-
   @ViewChild('chart')
   chart: any
 
-  constructor(private collectionsService: CollectionsService) {}
+  constructor(
+    private collectionsService: CollectionsService,
+    private graphsPageService: GraphsPageService
+  ) {}
 
   ngAfterViewInit(): void {
     this.collectionsService.getCollection(true).then(collection => {
@@ -39,48 +38,16 @@ export class GraphsPageComponent implements AfterViewInit{
     })
   }
 
-  yearsFromCollection(cards: UrzaCard[]) {
-    return cards
-      .map((card) => card.scryfallData?.released_at)  // Card -> Release date
-      .filter((date) => date !== undefined)           // Filter out undefined dates
-      .map((date) => parseInt(date!.split('-')[0]))   // Defined release date -> year as number
-      .reduce((acc, year) => {                        // Regroup by year
-        acc[year] = (acc[year] || 0) + 1;
-        return acc;
-      }, {} as { [year: number]: number })
-  }
-  
-  constructGraphData() {
-    if (!this.collection1 || !this.collection2) {
-      return
-    }
-
-    // Get dates
-    const dates1 = this.yearsFromCollection(this.collection1.cards)
-    const dates2 = this.yearsFromCollection(this.collection2.cards)
-
-    // Get min - max
-    const allYears = [...Object.keys(dates1), ...Object.keys(dates2)].map(yearStr => parseInt(yearStr));
-    const min = Math.min(...allYears)
-    const max = Math.max(...allYears)
-    
-    this.graphLabels = []
-    this.graphData1 = []
-    this.graphData2 = []
-    
-    for (let i = min; i <= max; i++) {
-      this.graphLabels.push(i.toString())
-      this.graphData1?.push(dates1[i] ?? 0)
-      this.graphData2?.push(dates2[i] ?? 0)
-    }
-  }
-
   constructChart() {
     if (!this.collection1 || !this.collection2) {
       return
     }
 
-    this.constructGraphData()
+    const graphData = this.graphsPageService.constructGraphData(this.collection1, this.collection2)
+
+    if (!graphData) {
+      return
+    }
 
     this.canvas = this.chart.nativeElement
     this.ctx = this.canvas.getContext('2d')
@@ -103,20 +70,20 @@ export class GraphsPageComponent implements AfterViewInit{
           datasets: [
             {
               label: this.collection1.file?.name,
-              data: this.graphData1,
+              data: graphData.graphData1,
               backgroundColor: backgroundColor,
               borderColor: primaryColor,
               fill: true
             },
             {
               label: this.collection2.file?.name,
-              data: this.graphData2,
+              data: graphData.graphData2,
               backgroundColor: backgroundColor,
               borderColor: secondaryColor,
               fill: true
             }
           ],
-          labels: this.graphLabels
+          labels: graphData.graphLabels
         }
       }
     )
---
+++ src/app/pages/graphs-page/graphs-page.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@angular/core";
+import { UrzaCard } from "../../data-models/urza-card.model";
+import { Collection } from "../../data-models/collection.model";
+
+@Injectable({ providedIn: 'root' })
+export class GraphsPageService {
+
+    constructGraphData(collection1: Collection, collection2: Collection) {
+        if (!collection1 || !collection2) {
+          return
+        }
+    
+        // Get dates
+        const dates1 = this.yearsFromCollection(collection1.cards)
+        const dates2 = this.yearsFromCollection(collection2.cards)
+    
+        // Get min - max
+        const allYears = [...Object.keys(dates1), ...Object.keys(dates2)].map(yearStr => parseInt(yearStr));
+        const min = Math.min(...allYears)
+        const max = Math.max(...allYears)
+        
+        let graphLabels: string[] = []
+        let graphData1: number[] = []
+        let graphData2: number[] = []
+        
+        for (let i = min; i <= max; i++) {
+          graphLabels.push(i.toString())
+          graphData1.push(dates1[i] ?? 0)
+          graphData2.push(dates2[i] ?? 0)
+        }
+
+        return {
+            graphLabels: graphLabels,
+            graphData1: graphData1,
+            graphData2: graphData2
+        }
+    }
+
+    yearsFromCollection(cards: UrzaCard[]) {
+      return cards
+        .map((card) => card.scryfallData?.released_at)  // Card -> Release date
+        .filter((date) => date !== undefined)           // Filter out undefined dates
+        .map((date) => parseInt(date!.split('-')[0]))   // Defined release date -> year as number
+        .reduce((acc, year) => {                        // Regroup by year
+            acc[year] = (acc[year] || 0) + 1;
+            return acc;
+        }, {} as { [year: number]: number })
+    }
+}
